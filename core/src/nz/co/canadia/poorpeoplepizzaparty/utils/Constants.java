9259ab6bd41a9c913c94e908b4c33f4a5c83e928
Ashley Noel Hinton <ashley@canadia.co.nz>
2018-04-24 21:00:37 +1200

    comment about green

--- core/src/nz/co/canadia/poorpeoplepizzaparty/utils/Constants.java
+++ core/src/nz/co/canadia/poorpeoplepizzaparty/utils/Constants.java
@@ -13,7 +13,7 @@ public class Constants {
     // Colours
     public static final Color BORDER_COLOUR = Color.BLACK;
     public static final Color BG_COLOUR =
-            new Color(48f/255, 184f/255, 73f/255, 1f);
+            new Color(48f/255, 184f/255, 73f/255, 1f); // green, it's green
 
     // App dimensions
     public static final int APP_WIDTH = 960;
