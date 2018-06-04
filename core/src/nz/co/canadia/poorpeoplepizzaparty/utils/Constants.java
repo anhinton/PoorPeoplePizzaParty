933ae2baa69f8ce29c90ee2599efeade46a9b6ff
Ashley Noel Hinton <ashley@canadia.co.nz>
2018-06-05 11:20:21 +1200

    font change

--- core/src/nz/co/canadia/poorpeoplepizzaparty/utils/Constants.java
+++ core/src/nz/co/canadia/poorpeoplepizzaparty/utils/Constants.java
@@ -39,8 +39,8 @@ public class Constants {
     public static final int PIZZA_TOP = BASE_X + BASE_HEIGHT;
 
     // Topping menu button size
-    public static final int BUTTON_WIDTH = 380;
-    public static final int BUTTON_HEIGHT = 100;
+    public static final int BUTTON_WIDTH = 300;
+    public static final int BUTTON_HEIGHT = 80;
 
     // Topping menu UI padding/spacing
     public static final int MENU_PADDING = 20;
