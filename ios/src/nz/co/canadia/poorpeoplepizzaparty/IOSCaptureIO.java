ce95d1dae65b27c4852f4c536f1b8eff85fbab39
Ashley Noel Hinton <ashley@canadia.co.nz>
2020-05-12 21:43:06 +1200

    copy android pizza xml code to IOSCaptureIO methods

--- ios/src/nz/co/canadia/poorpeoplepizzaparty/IOSCaptureIO.java
+++ ios/src/nz/co/canadia/poorpeoplepizzaparty/IOSCaptureIO.java
@@ -1,6 +1,10 @@
 package nz.co.canadia.poorpeoplepizzaparty;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+
 import nz.co.canadia.poorpeoplepizzaparty.utils.CaptureIO;
+import nz.co.canadia.poorpeoplepizzaparty.utils.Constants;
 
 public class IOSCaptureIO implements CaptureIO {
     @Override
@@ -10,11 +14,17 @@ public class IOSCaptureIO implements CaptureIO {
 
     @Override
     public void savePizzaXml(String pizzaXml) {
-
+        FileHandle autosaveFile = Gdx.files.local(Constants.autosaveFile);
+        autosaveFile.writeString(pizzaXml, false);
     }
 
     @Override
     public String loadPizzaXml() {
-        return null;
+
+        if (Gdx.files.local(Constants.autosaveFile).exists()) {
+            return Gdx.files.local(Constants.autosaveFile).readString();
+        } else {
+            return "";
+        }
     }
 }
