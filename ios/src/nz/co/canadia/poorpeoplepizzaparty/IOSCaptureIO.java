994a0bca0e9c2aaf6908ccf853cded1989c04a07
Ashley Noel Hinton <ashley@canadia.co.nz>
2020-05-12 15:56:49 +1200

    add dummy IOCaptureIO

---
+++ ios/src/nz/co/canadia/poorpeoplepizzaparty/IOSCaptureIO.java
@@ -0,0 +1,20 @@
+package nz.co.canadia.poorpeoplepizzaparty;
+
+import nz.co.canadia.poorpeoplepizzaparty.utils.CaptureIO;
+
+public class IOSCaptureIO implements CaptureIO {
+    @Override
+    public void savePostcardImage(Postcard postcard) {
+
+    }
+
+    @Override
+    public void savePizzaXml(String pizzaXml) {
+
+    }
+
+    @Override
+    public String loadPizzaXml() {
+        return null;
+    }
+}
