4bc64ac0b609da0fd8aee95e21efa80d44772b97
Ashley Noel Hinton <ashley@canadia.co.nz>
2021-12-31 11:45:21 +1300

    Upgrade to lwjgl3

--- desktop/src/nz/co/canadia/poorpeoplepizzaparty/desktop/DesktopLauncher.java
+++ desktop/src/nz/co/canadia/poorpeoplepizzaparty/desktop/DesktopLauncher.java
@@ -1,24 +1,22 @@
 package nz.co.canadia.poorpeoplepizzaparty.desktop;
 
-import com.badlogic.gdx.Files;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import nz.co.canadia.poorpeoplepizzaparty.PoorPeoplePizzaParty;
 import nz.co.canadia.poorpeoplepizzaparty.utils.Constants;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = Constants.GAME_NAME;
-		config.width = Constants.DESKTOP_WIDTH;
-		config.height = Constants.DESKTOP_HEIGHT;
-		config.resizable = false;
-		config.addIcon("icon_128.png", Files.FileType.Local);
-		config.addIcon("icon_32.png", Files.FileType.Local);
-		config.addIcon("icon_16.png", Files.FileType.Local);
-		new LwjglApplication(
-		        new PoorPeoplePizzaParty(new DesktopCaptureIO()),
-				config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle(Constants.GAME_NAME);
+		config.setWindowedMode(Constants.DESKTOP_WIDTH, Constants.DESKTOP_HEIGHT);
+		config.setResizable(false);
+		config.setWindowIcon(
+				"icon_128.png",
+				"icon_32.png",
+				"icon_16.png"
+		);
+
+		new Lwjgl3Application(new PoorPeoplePizzaParty(new DesktopCaptureIO()), config);
 	}
 }
